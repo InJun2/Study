274f794f5411974f8121578e969017b802723a8f
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-09-22 22:06:05 +0900

    Update Professor.java

--- ArrayList/Study1/Professor.java
+++ ArrayList/Study1/Professor.java
@@ -10,11 +10,11 @@ public class Professor {
 	private Scanner sc = new Scanner(System.in);
 	
 	public void setInfo() {
-		System.out.printf("³ªÀÌ :");
+		System.out.printf("ë‚˜ì´ :");
 		this.age = sc.nextInt();
-		System.out.printf("ÀÌ¸§ :");
+		System.out.printf("ì´ë¦„ :");
 		this.name = sc.next();
-		System.out.printf("°ú¸ñ :");
+		System.out.printf("ê³¼ëª© :");
 		this.subject = sc.next();
 	}
 	
@@ -23,7 +23,7 @@ public class Professor {
 	}
 	
 	public void getInfo() {
-		System.out.println("³ªÀÌ : "+age+ "\t ÀÌ¸§: "+ name +"\t °ú¸ñ: "+subject);
+		System.out.println("*êµìˆ˜ ì •ë³´* \në‚˜ì´ : "+age+ "   ì´ë¦„: "+ name +"   ê³¼ëª©: "+subject);
 	}
 	
 }
