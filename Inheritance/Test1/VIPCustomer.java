1ff3e0b3695bc7ceaeaa54524cd12503e76970b1
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-09-23 18:32:12 +0900

    Update VIPCustomer.java

--- Inheritance/Test1/VIPCustomer.java
+++ Inheritance/Test1/VIPCustomer.java
@@ -3,13 +3,25 @@ package kr.or.kh02;
 public class VIPCustomer extends Customer{
 	private int agentId;
 	private double salesRadio;
+	private static int agentNum=1000;
 	
+	public VIPcustomer() {
+		super();
+		setCustomerGrade("VIP");
+		setBonusRatio(0.05);
+		setSalesRadio(0.1);
+		System.out.println("VIPCustomer() 생성자 호출");
+	}
 	public VIPCustomer(int customerId, String customerName) {
 		super(customerId, customerName);
 		setCustomerGrade("VIP");
 		setBonusRatio(0.05);
 		setSalesRadio(0.1);
-		
+		System.out.println("VIPCustomer(int,String) 생성자 호출");
+	}
+	
+	public void setAgent() {
+		this.agentID=agentNum++;
 	}
 	
 	public int getAgentId() {
