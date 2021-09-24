78cc60f3fc5dbbc1925f54a845dce6083df2f0f3
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-09-24 20:13:12 +0900

    Update ManGongupSchool.java

--- Inheritance/Test2/ManGongupSchool.java
+++ Inheritance/Test2/ManGongupSchool.java
@@ -3,10 +3,12 @@ package kr.or.kh02;
 import java.util.Scanner;
 
 public class ManGongupSchool extends ManSchool {
+	private static final Scanner scanner = new Scanner(System.in);
 	private int gongup;
 
 	public String manGongupInfo() {
-		return "국어 : " + getKor() + "영어 : " + getEng() + "수학 : " + getMath() + " 기술: " + getGis()+" 공업: " + getGongup();
+		return "국어 : " + getKor() + " 영어 : " + getEng() + " 수학 : " + getMath() + " 기술: " + getGis()+" 공업: " + getGongup()
+		+ " 총점 : " + total() + " 평균 : " + avg();
 	}
 	
 	public int getGongup() {
@@ -17,8 +19,15 @@ public class ManGongupSchool extends ManSchool {
 		this.gongup = gongup;
 	}
 	
-	protected static void inputManGongupSchool(Scanner scanner) {
-		ManGongupSchool mansgongupschool = new ManGongupSchool();
+	public int total() {
+		return super.total()+gongup;
+	}
+
+	public double avg() {
+		return total()/5.0;
+	}
+	
+	protected static void inputManGongupSchool(ManGongupSchool mansgongupschool) {
 		System.out.print("국어 : ");
 		mansgongupschool.setKor(scanner.nextInt());
 		System.out.print("영어 : ");
@@ -29,7 +38,6 @@ public class ManGongupSchool extends ManSchool {
 		mansgongupschool.setGis(scanner.nextInt());
 		System.out.print("공업 : ");
 		mansgongupschool.setGongup(scanner.nextInt());
-		System.out.println(mansgongupschool.manGongupInfo());
 	}
 	
 }
