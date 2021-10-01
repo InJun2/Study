f8b0bdc44742e8ffe1f9bc23c9880b5fe39b7e3e
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-10-01 21:42:53 +0900

    Update VIPCustomer.java

--- Inheritance/Test1/VIPCustomer.java
+++ Inheritance/Test1/VIPCustomer.java
@@ -20,7 +20,6 @@ public class VIPCustomer extends Customer{
 		System.out.println("VIPCustomer(int,String) 생성자 호출");
 	}
 	
-	@Override
 	public int calcPrice(int price) {
 		bonusPoint += price * bonusRatio;
 		return price - (int)(price * saleRatio);
