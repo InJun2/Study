93780e91aaad7d89d18b21c5d8993e6fcb6d3f39
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-10-05 18:41:30 +0900

    Update MainTest.java

--- ArrayList/Study2/MainTest.java
+++ ArrayList/Study2/MainTest.java
@@ -4,7 +4,6 @@ public class MainTest {
 	
 	public static void main(String[] args) {
 		new InputInfo();
-		new InputInfo();
 		new OutputInfo();
 		OutputInfo.OutputCustomerInfo(CustomerList.getCustomers());
 	}
