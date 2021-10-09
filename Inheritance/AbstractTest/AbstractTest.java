28a51595a41e931f59c374102c74be1e447bafc1
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-10-09 18:47:14 +0900

    주석추가

--- Inheritance/AbstractTest/AbstractTest.java
+++ Inheritance/AbstractTest/AbstractTest.java
@@ -1,22 +1,18 @@
 package Test;
 
 public class AbstractTest extends Exam {
-
 	public void sayHi() {
 		System.out.println("안녕?");
 	}
-
-
 	public void printSum(int a, int b) {
 		int result = sum(a,b);
 		System.out.println(a + "과(와) " + b + "의 합은 " + result + "(이)야");
-	}
+	}   //추상메소드이기에 구현해두어야함
+
 
 	public static void main(String[] args) {
-
 		// 하위 클래스인 AbstractTest 객체 생성
 		AbstractTest at = new AbstractTest();
-
 
 		at.sayHi(); 
 		at.printSum(30, 40); 
