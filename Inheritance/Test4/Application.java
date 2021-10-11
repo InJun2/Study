7a3338534b581a214c86ee56d9ce4b54f34b1bd4
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-10-11 20:46:15 +0900

    Add files via upload

---
+++ Inheritance/Test4/Application.java
@@ -0,0 +1,25 @@
+package inherit;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Circle[] cir = new Circle[2];
+		Rectangle[] rec = new Rectangle[2];
+		
+		cir[0] = new Circle(1, 2, 3);
+		cir[1] = new Circle(3, 3, 4);
+		
+		rec[0] = new Rectangle(-1, -2, 5, 2);
+		rec[1] = new Rectangle(-2, 5, 2, 8);
+		
+		System.out.println("===== circle =====");
+		for(Circle c : cir) {
+			c.draw(); // 
+		}
+		
+		System.out.println("===== rectangle =====");
+		for(Rectangle r : rec) {
+			r.draw(); // 
+		}
+	}
+}
