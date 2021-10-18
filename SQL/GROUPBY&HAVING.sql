838b7247a8a14803d682da043c4074cdd9139d61
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-10-18 15:45:16 +0900

    Update GROUPBY&HAVING.sql

--- SQL/GROUPBY&HAVING.sql
+++ SQL/GROUPBY&HAVING.sql
@@ -70,11 +70,9 @@ ORDER BY 1, 2;
         1: FROM        조회하고자 하는 테이블명
         2: WHERE       조건식
         3: GROUP BY    그룹 기준에 해당하는 컬럼명 | 계산식 | 함수식
-        4: HAVING      그룹에 대한 조건식                               -- 그룹에 대한 where 절이라고 생각하면 됨
+        4: HAVING      그룹에 대한 조건식
         6: ORDER BY    정렬 기준에 해당하는 컬럼명 | 별칭 | 컬럼 순번
 */
-
-
 -- 각 부서별 평균 급여 조회
 SELECT DEPT_CODE, FLOOR(AVG(NVL(SALARY, 0)))
 FROM EMPLOYEE
@@ -88,17 +86,55 @@ WHERE SALARY >= 3000000
 GROUP BY DEPT_CODE
 ORDER BY DEPT_CODE;
 
+-- 각 부서별 평균 급여가 300만원 이상인 부서들만 조회
+SELECT DEPT_CODE, FLOOR(AVG(NVL(SALARY, 0)))
+FROM EMPLOYEE
+--WHERE FLOOR(AVG(NVL(SALARY, 0))) >= 3000000 -- 에러 발생
+GROUP BY DEPT_CODE
+HAVING FLOOR(AVG(NVL(SALARY, 0))) >= 3000000
+ORDER BY DEPT_CODE;
 
+-- 직급별 총 급여의 합이 10000000 이상인 직급들만 조회
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+HAVING SUM(SALARY) >= 10000000
+ORDER BY JOB_CODE;
 
+-- 부서별 보너스를 받는 사원이 없는 부서들만 조회
+SELECT DEPT_CODE, COUNT(BONUS)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING COUNT(BONUS) = 0
+ORDER BY DEPT_CODE;
 
+/*
+    <집계 함수>
+        그룹별 산출한 결과 값의 중간 집계를 계산 해주는 함수
+*/
+-- 직급별 급여의 합계를 조회
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+ORDER BY JOB_CODE;
 
+-- 마지막 행에 전체 총 급여의 합계까지 조회
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY ROLLUP(JOB_CODE)
+ORDER BY JOB_CODE;
 
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY CUBE(JOB_CODE)
+ORDER BY JOB_CODE;
 
+-- 부서 코드도 같고 직급 코드도 같은 사원들을 그룹 지어서 급여의 합계를 조회
+SELECT DEPT_CODE, JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE, JOB_CODE
+ORDER BY DEPT_CODE, JOB_CODE;
 
+-- ROLLUP(컬럼 1, 컬럼 2, ...) -> 컬럼 1을 가지고 중간집계를 내는 함수
 
-
-
-
-
-
-
+-- CUBE(컬럼 1, 컬럼 2, ...) -> 컬럼 1을 가지고 중간집계를 내고, 컬럼 2를 가지고 중간집계를 낸다. 즉, 전달되는 컬럼 모두 집계하는 함수
