0fad31602301e76c4f3526c51122e3bc28710649
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-01-14 21:50:05 +0900

    22-01-14
    - pom.xml에서 maver을 이용한 ojdbc8 설치 및 연결 적용 확인

---
+++ Spring/wsRepo/spring07prjDB/src/main/java/com/kh/app07/contoller/TestController.java
@@ -0,0 +1,19 @@
+package com.kh.app07.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/test")
+	public String test(){
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("½ÇÆÐ");
+		}
+		System.out.println("success");
+		return"redirect:/";
+	}
+}
