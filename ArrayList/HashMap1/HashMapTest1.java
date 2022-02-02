efebb11e4afdfae48f1c46401e3ee6fceb64d7a5
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-02-02 23:06:58 +0900

    HashMapTest1
    - 주석 추가

--- ArrayList/HashMap1/HashMapTest1.java
+++ ArrayList/HashMap1/HashMapTest1.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 public class HashMapTest {
 
 	public static void main(String[] args) {
+		//  key와 value가 String인 HashMap 으로 구성된 ArrayList 생성
 		ArrayList<HashMap<String,String>> haksaList = new ArrayList<HashMap<String,String>>();
 		HashMap<String,String> hashMap = new HashMap<>();
+		// HashMap은 key와 밸류로 구성, 생성 시 키와 밸류 타입 명시하여 생성
 		HashMap<String,String> hashMap2 = new HashMap<>();
 		hashMap.put("age","25");
 		hashMap.put("name", "HwangInJun");
