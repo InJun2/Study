621222ea0fd5c8b0e60f1495b1ae5df9e8604f90
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-02-03 21:06:53 +0900

    22-02-03
    - spring log사용 ( lombok )

---
+++ Spring/wsRepo/spring21prj/src/main/java/com/kh/app21/Vo.java
@@ -0,0 +1,9 @@
+package com.kh.app21;
+
+import lombok.Data;
+
+@Data
+public class Vo {
+	private String name;
+	private String pwd;
+}
---
+++ Spring/wsRepo/spring21prj/src/test/java/com/kh/app21/TestClass.java
@@ -0,0 +1,21 @@
+package com.kh.app21;
+
+import org.junit.Test;
+import org.slf4j.Logger;			// slf4j class »ç¿ë
+import org.slf4j.LoggerFactory;
+
+import lombok.extern.slf4j.Slf4j;	
+
+@Slf4j
+public class TestClass {
+	
+	@Test
+	public void test() {
+//		Logger logger = LoggerFactory.getLogger(TestClass.class);
+		log.trace("trace");
+		log.debug("debug");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
+	}
+}
