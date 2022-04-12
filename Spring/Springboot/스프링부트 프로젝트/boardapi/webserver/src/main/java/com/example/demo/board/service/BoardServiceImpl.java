d5d64dc543e798a91d53684274fb5938849f6b67
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-04-12 16:59:31 +0900

    22-04-12
    - 사설 SSL 인증서 생성 및  https 프로토콜 이용 ( PKCS12 )
    - BoardAop 오류 해결
    - apiserver로 요청도 https 요청으로 변경 ( HttpsURLConnection 이용), 및 신뢰받지 못하는 인증서로 우회하여 사용

--- Spring/Springboot/스프링부트 프로젝트/boardapi/webserver/src/main/java/com/example/demo/board/service/BoardServiceImpl.java
+++ Spring/Springboot/스프링부트 프로젝트/boardapi/webserver/src/main/java/com/example/demo/board/service/BoardServiceImpl.java
@@ -6,8 +6,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.json.JSONArray;
 import org.springframework.stereotype.Service;
@@ -20,10 +31,11 @@ import com.google.gson.Gson;
 @Service
 public class BoardServiceImpl implements BoardService{
 	
+	final static String BOARD_API_SERVER = "https://localhost:8446/board/";
 	
 	@Override
 	public List<BoardDto> loadBoardList() throws Exception {
-		JSONArray jsonArr = new JSONArray(requestBoardApi("board/list", "GET"));	// jsonArray 형태의 스트링값을 받아와서 JsonArray로 변환 ( JSONArray는 List<HashMap<K,Y>> 형태 인듯 함 )
+		JSONArray jsonArr = new JSONArray(requestBoardApi("list", "GET"));	// jsonArray 형태의 스트링값을 받아와서 JsonArray로 변환 ( JSONArray는 List<HashMap<K,Y>> 형태 인듯 함 )
 		List<BoardDto> boardList = new ArrayList<BoardDto>();
 		for(int i = 0; i< jsonArr.length(); i++) {	// for문 없이 boardDto를 파싱해서 컬렉션에 담을수는 없는지 ? 
 			Gson gson = new Gson();
@@ -36,49 +48,49 @@ public class BoardServiceImpl implements BoardService{
 	
 	@Override
 	public BoardDto selectBoardDetail(String boardNo) throws Exception {
-		String jsonStr = requestBoardApi("board/detail/"+boardNo, "GET");
+		String jsonStr = requestBoardApi("detail/"+boardNo, "GET");
 		return new ObjectMapper().readValue(jsonStr, BoardDto.class);	// jackson 역직렬화 ( json string -> POJO ) 
 	}
 
 	@Override
 	public int insertBoard(BoardDto dto) throws Exception {
-		return parseInt(requestBoardApi("board/insert","POST", dto));
+		return parseInt(requestBoardApi("insert","POST", dto));
 	}
 	
 	@Override
 	public int deleteBoard(String deleteNo) throws Exception {
-		return parseInt(requestBoardApi("board/delete/"+deleteNo, "GET"));
+		return parseInt(requestBoardApi("delete/"+deleteNo, "GET"));
 	}	
 	
 	@Override
 	public int deleteUdminBoard(String deleteNoArr) throws Exception {
-		return parseInt(requestBoardApi("board/delete/admin/"+deleteNoArr, "DELETE"));
+		return parseInt(requestBoardApi("delete/admin/"+deleteNoArr, "DELETE"));
 	}
 
 	@Override
-	public int updateBoard(BoardDto dto) throws IOException {
-		String result = requestBoardApi("board/update", "POST", dto);
+	public int updateBoard(BoardDto dto) throws Exception {
+		String result = requestBoardApi("update", "POST", dto);
 		return parseInt(result);
 	}
 	
 	
 	// 밑 메소드는 요청 관련 혹은 타입 변환 메소드
-	private String requestBoardApi(String uri, String method) throws IOException{
-		HttpURLConnection conn = settingConnection(uri, method);		// http 요청을 위해 특정 uri에 특정 method 연결을 위한 세팅
+	private String requestBoardApi(String uri, String method) throws Exception{
+		HttpsURLConnection conn = settingConnection(uri, method);		// http 요청을 위해 특정 uri에 특정 method 연결을 위한 세팅
 		return inputReadResponse(conn);		// 해당 uri에 세팅에 맞게 보낸 요청의 결과 읽어오기
 	}
 	
-	private String requestBoardApi(String uri, String method, Object obj) throws IOException{
-		HttpURLConnection conn = settingConnection(uri, method);		// http 요청을 위해 특정 uri에 특정 method 연결을 위한 세팅
+	private String requestBoardApi(String uri, String method, Object obj) throws Exception{
+		HttpsURLConnection conn = settingConnection(uri, method);		// http 요청을 위해 특정 uri에 특정 method 연결을 위한 세팅
 		conn = setParameterConnection(conn, obj);
 		return inputReadResponse(conn);		// 해당 uri에 세팅에 맞게 보낸 요청의 결과 읽어오기
 	}
 	
-	private HttpURLConnection settingConnection(String uri, String method) throws IOException {
+	private HttpsURLConnection settingConnection(String uri, String method) throws Exception {
 		try {
-//			System.setProperty("https.protocols", "PKCS12");	// ssl 보류
-			URL url = new URL("http://localhost:8446/"+uri);	// public static으로 선언해서 다른데서도 적용할지, 그냥 Bean 등록?
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			URL url = new URL(BOARD_API_SERVER + uri);	// 그냥 Bean 등록 할지?\
+			ingoreSsl();
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 			conn.setRequestMethod(method);	// 메소드 타입 유효성 검사도 해당 메소드 안에 존재
 			conn.setRequestProperty("Content-type", "application/json");
 			conn.setDoInput(true);
@@ -91,7 +103,8 @@ public class BoardServiceImpl implements BoardService{
 	}
 	
 	
-	private String inputReadResponse(HttpURLConnection conn) throws IOException {
+
+	private String inputReadResponse(HttpsURLConnection conn) throws IOException {
 		try {
 			StringBuffer sb = new StringBuffer();
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
@@ -109,7 +122,7 @@ public class BoardServiceImpl implements BoardService{
 		}
 	}
 	
-	private HttpURLConnection setParameterConnection(HttpURLConnection conn, Object obj) throws IOException {		// apiserver에 파라미터 전송 목적
+	private HttpsURLConnection setParameterConnection(HttpsURLConnection conn, Object obj) throws IOException {		// apiserver에 파라미터 전송 목적
 		conn.setDoOutput(true);
 		try (DataOutputStream output = new DataOutputStream(conn.getOutputStream())) {
 			
@@ -134,5 +147,48 @@ public class BoardServiceImpl implements BoardService{
 	private int parseInt(String str) {
 		return Integer.parseInt(str);
 	}
+
+	private static void ingoreSsl() throws KeyManagementException, NoSuchAlgorithmException{				// 아래 부터는 ssl 우회하기 위한 메소드 ( 구조 모름 )
+		HostnameVerifier hv = new HostnameVerifier() {
+			public boolean verify(String urlHostName, SSLSession session) {
+				return true;
+			}
+		};
+		trustAllHttpsCertificates();	// 모든 htts 인증서 신뢰
+		HttpsURLConnection.setDefaultHostnameVerifier(hv);	// 호스트 이름 기본 확인자 설정
+	}
+	
+	private static void trustAllHttpsCertificates() throws NoSuchAlgorithmException, KeyManagementException {
+		TrustManager[] trustAllCerts = new TrustManager[1];
+		TrustManager tm = new miTM();
+		trustAllCerts[0] = tm;
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, null);
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	}
 	
 }
+	class miTM implements TrustManager, X509TrustManager{
+		 public X509Certificate[] getAcceptedIssuers() {
+	        return null;
+	     }
+	 
+	     public boolean isServerTrusted(X509Certificate[] certs) {
+	        return true;
+	     }
+	 
+	     public boolean isClientTrusted(X509Certificate[] certs) {
+	        return true;
+	     }
+	 
+	     public void checkServerTrusted(X509Certificate[] certs, String authType)
+	    	throws CertificateException {
+	    	 	return;
+	      }
+	 
+	     public void checkClientTrusted(X509Certificate[] certs, String authType)
+	        throws CertificateException {
+	    	 	return;
+	     }
+
+	}
