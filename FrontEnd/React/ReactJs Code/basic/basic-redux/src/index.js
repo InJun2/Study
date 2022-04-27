6d9ca8b1c1a8f71c7570e39b1c1628767c401f22
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-04-27 16:27:24 +0900

    22-04-27
    - 주석 추가
    - 함수형 컴포넌트기반의 redux toolkit과 Hook을 이용한 새로운 React 예제프로젝트 생성 예정

--- FrontEnd/React/ReactJs Code/basic/basic-redux/src/index.js
+++ FrontEnd/React/ReactJs Code/basic/basic-redux/src/index.js
@@ -9,8 +9,8 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 
-const store = legacy_createStore(reducers);        // configureStore 로 변경 예정 ( 개선버전 )
-
+const store = legacy_createStore(reducers);        // 현재 해당 react 프로젝트는 인프런 강의 예제코드로 class형 컴포넌트 사용 -> 현재는 대부분 함수형 컴포넌트를 사용하고 현재 지원하는 라이브러리에 차이가 존재
+                                                   // redux toolkit와 React Hook을 이용한 새로운 예제 프로젝트 생성 예정
 store.subscribe(() => console.log(store.getState()));
 store.dispatch(actions.increment());
 store.dispatch(actions.decrement());
