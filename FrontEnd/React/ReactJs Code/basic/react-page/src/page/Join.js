76a84df3778453010e4fd3e86794e1b7d8aef33f
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-05-23 17:47:16 +0900

    22-05-23
    - 회원가입 도로명 주소 api 사용
    - 회원가입 input 유효성 검사 구현 예정

--- FrontEnd/React/ReactJs Code/basic/react-page/src/page/Join.js
+++ FrontEnd/React/ReactJs Code/basic/react-page/src/page/Join.js
@@ -1,15 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import PopupPostCode from '../component/DaumAddress';
+import { IdValidation } from '../component/JoinValidation';
+import PopupDom from '../component/PopupDom';
 import '../css/join.css';
 
 function Join(){
-    const [gender, setGender] = useState("");
     
-    const genderChange = (e) =>{
-        setGender(e.target.value);
-        console.log(e.target.value);
+    const [inputs, setInputs] = useState({
+        id: '',
+        password: '',
+        passwordCheck: '',
+        name: '',
+        birthYear: '',
+        birthMonth: '',
+        birthDay: '',
+        gender: '',
+        detailAddress: '',
+        email: '',
+        phone: ''
+    });
+
+    const { id, password, passwordCheck, name, birthYear, birthMonth, birthDay, gender, detailAddress, email, phone } = inputs;    // 비구조화 할당 ( address 제외)
+    
+    const onChange = (e) =>{
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]:value
+        });
     };
 
-    const addressSearch = (e) =>{
+    const [isBooleans, setIsBooleans] = useState({
+        isId: false,
+        isPassword: false,
+        isPasswordCheck: false,
+        isName: false,
+        isBirthYear: false,
+        isBirthMonth: false,
+        isBirthDay: false,
+        isGender: false,
+        isAddress: false,
+        isEmail: false,
+        isPhone: false
+    })
+
+    const { isId, isPassword, isPasswordCheck, isName, isBirthYear, isBirthMonth, isBirthDay, isGender, isAddress, isEmail, isPhone} = isBooleans;
+
+    const [address, setAddress] = useState(""); 
+    const [addressPopup, setAddressPopup] = useState(false);
+
+    const addressInput = useRef(""); 
+
+    useEffect(()=>{
+        addressInput.current.value = address;
+    }, [address])
+
+    const openAddress = () => {
+        setAddressPopup(true)
+    }
+    const closeAddress = () => {
+        setAddressPopup(false)
     }
 
     return(
@@ -19,7 +69,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Id</span>
                         <span className='pull_input_span'>
-                            <input type='text' required/>
+                            <input type='text' onChange={onChange} name='id' value={id} onBlur={IdValidation}/>
                         </span>
                         <span className='join_error_text'>필수정보입니다</span>
                     </div>
@@ -27,7 +77,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Pwd</span>
                         <span className='pull_input_span'>
-                            <input type='password'/>
+                            <input type='password' onChange={onChange} name='password' value={password}/>
                         </span>
                         <span className='join_error_text'>8~16자 영문 대 소문자, 숫자 사용</span>
                     </div>
@@ -35,7 +85,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Pwd Check</span>
                         <span className='pull_input_span'>
-                            <input type='password'/>
+                            <input type='password' onChange={onChange} name='passwordCheck' value={passwordCheck}/>
                         </span>
                         <span className='join_error_text'>비밀번호가 일치하지 않습니다</span>
                     </div>
@@ -43,7 +93,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Name</span>
                         <span className='pull_input_span'>
-                            <input type='text'/>
+                            <input type='text' onChange={onChange} name='name' value={name}/>
                         </span>
                         <span className='join_error_text'>한글 혹은 영문 사용</span>
                     </div>
@@ -52,13 +102,13 @@ function Join(){
                         <span className='join_title'>Birthday</span>
                         <div className='join_birthday_div'>
                             <span>
-                                <input type='text' placeholder='연도(4자)'/> 
+                                <input type='text' onChange={onChange} name='birthYear' value={birthYear} placeholder='연도(4자)'/> 
                             </span>
                             <span>
-                                <input type='text' placeholder='월'/>
+                                <input type='text' onChange={onChange} name='birthMonth' value={birthMonth} placeholder='월'/>
                             </span>
                             <span>
-                                <input type='text' placeholder='일'/>
+                                <input type='text' onChange={onChange} name='birthDay' value={birthDay} placeholder='일'/>
                             </span>
                         </div>
                         <span className='join_error_text'>잘못된 생일 정보 입니다</span>
@@ -67,7 +117,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Gender</span>
                         <span className='pull_input_span'>
-                            <select onChange={genderChange} value={gender}>
+                            <select onChange={onChange} name='gender' value={gender}>
                                 <option value="M">남자</option>
                                 <option value="F">여자</option>
                             </select>
@@ -77,7 +127,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Email</span>
                         <span className='pull_input_span'>
-                            <input type='email'/>
+                            <input type='email' onChange={onChange} name='email' value={email}/>
                         </span>
                         <span className='join_error_text'>잘못된 이메일 정보 입니다</span>
                     </div>
@@ -85,12 +135,21 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Address</span>
                         <div className='join_address'>
-                            <span>
-                                <input type='text'/>
+                            <span className='input_span_readonly'>
+                                <input type='text' ref={addressInput} readOnly/>
                             </span>
-                            <button onClick={addressSearch}>주소찾기</button>
-                            <span className='pull_input_span_readonly'>
-                                <input type='text' readOnly/> 
+                            <button onClick={openAddress}>주소찾기</button>
+                            <div id="popupDom" className='addressPopup'>
+                                {addressPopup && 
+                                    (
+                                        <PopupDom>
+                                            <PopupPostCode onClose={closeAddress} setAddress={setAddress}/>
+                                        </PopupDom>
+                                    )}
+                            </div>
+                            <span className='join_title'>Detail Address</span>
+                            <span className='pull_input_span'>
+                                <input type='text' onChange={onChange} name='detailAddress' value={detailAddress}/> 
                             </span>
                             <span className='join_error_text'>주소가 입력되지 않았습니다</span>
                         </div>
@@ -99,7 +158,7 @@ function Join(){
                     <div className='join_input_row'>
                         <span className='join_title'>Phone</span>
                         <span className='pull_input_span'>
-                            <input type='text'/>    
+                            <input type='text' onChange={onChange} name='phone' value={phone}/>    
                         </span>
                         <span className='join_error_text'>000-0000-0000 형식이여야 합니다</span>
                     </div>
