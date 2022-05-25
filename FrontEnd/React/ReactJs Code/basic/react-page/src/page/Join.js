41b4deef76a847665f6e724a61c7d59938936181
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-05-25 17:25:43 +0900

    22-05-25
    - 회원가입 유효성 검사 처리 로직 추가
    - address 유효성 검사, 회원가입 컴포넌트로 분해 예정

--- FrontEnd/React/ReactJs Code/basic/react-page/src/page/Join.js
+++ FrontEnd/React/ReactJs Code/basic/react-page/src/page/Join.js
@@ -5,8 +5,8 @@ import PopupDom from '../component/PopupDom';
 import '../css/join.css';
 
 function Join(){
-    const { register, handleSubmit, getValues, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data); // 모든 input 유효성 검사완료시 해당 데이터 확인 후 join 진행 예정
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm({mode: "onBlur"});
+    const onSubmit = data => address&&console.log(data); // 모든 input 유효성 검사완료시 해당 데이터 확인 후 join 진행 예정
 
     const [address, setAddress] = useState(""); 
     const [addressPopup, setAddressPopup] = useState(false);
@@ -24,6 +24,44 @@ function Join(){
         setAddressPopup(false)
     }
 
+    const autoHyphen = (e) =>{
+        e.target.value = e.target.value.replace(/[^0-9]/g, '')
+        .replace(/^(\d{3})(\d{3,4})(\d{4})$/g, "$1-$2-$3");
+    }
+
+    const getData = (value) =>{
+        const getValue = getValues(value);
+        return getValue;
+    }
+
+    const numberMaxLength = (e) =>{
+        if(e.target.value.length > e.target.maxLength){
+            e.target.value = e.target.value.slice(0, e.target.maxLength);
+        }
+    }
+
+    const birthDayValidate = (value) =>{
+        const year = parseInt(getData("birthYear"));
+        const month = parseInt(getData("birthMonth"));
+
+        if(month === 2){
+            if((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0))
+                return value<=29 && value>=1
+            else
+                return value<=28 && value>=1
+        }
+        else if(month ===4 || month ===6 || month ===9 || month===11){
+            return value<=30 && value>=1
+        }
+        else{
+            return value<=31 && value>=1
+        }
+    }
+
+    const checkInteger = (value) =>{
+        return value %1 ===0;
+    }
+
     return(
         <div className='join_container'>
             <div className='join_box'>
@@ -36,7 +74,7 @@ function Join(){
                                     required: {value: true, message: "ID 입력은 필수 입니다"},
                                     maxLength: {value: 12, message: "ID의 최대길이는 12자 입니다"},
                                     minLength : {value: 6, message: "ID의 최소길이는 6자 입니다" },
-                                    pattern: {value:/^[A-Za-z0-9]{6,12}/, message: "영문자와 숫자를 조합해야 합니다"}
+                                    pattern: {value:/^(?=.*[A-Za-z])[A-Za-z0-9]{6,12}/, message: "영문자이거나 영문자,숫자 조합이여야 합니다"}
                                 })} placeholder="영문자 숫자를 포함한 6~12자 입력"/>
                             </span>
                             {errors.id && <span className='join_error_text'>{errors.id.message}</span>}
@@ -61,7 +99,7 @@ function Join(){
                             <input type='password' {...register("passwordCheck",{
                                 required: {value: true, message: "PWD를 다시 입력해주세요"},
                                 validate: {passwordChecked: value =>
-                                    { const {password} =getValues(); return password === value || "PWD가 동일하지 않습니다";}
+                                    { return getData("password") === value || "PWD가 동일하지 않습니다";}
                                 }
                             })} placeholder="비번 재입력"/>
                             </span>
@@ -85,22 +123,36 @@ function Join(){
                             <span className='join_title'>Birthday</span>
                             <div className='join_birthday_div'>
                                 <span>
-                                    <input type='number' min={1922} max={2022} placeholder='연도(4자)'/> 
+                                    <input type='number' placeholder='연도(4자)' maxLength="4" onInput={numberMaxLength} {...register("birthYear",{
+                                        required: { value: true, message: "태어난 연도를 입력해주세요"},
+                                        validate: { checkBirthYear :value => { return (value>new Date().getFullYear()-101 && value < new Date().getFullYear()-8) ||"잘못된 생년 정보 입니다"},
+                                                    checkInteger: value => checkInteger(value) ||"잘못된 생년 정보 입니다"}
+                                    })}/> 
                                 </span>
                                 <span>
-                                    <input type='number' min={1} max={12} placeholder='월'/>
+                                    <input type='number' placeholder='월' maxLength="2" onInput={numberMaxLength} {...register("birthMonth",{
+                                        required: { value: true, message: "태어난 월을 입력해주세요"},
+                                        validate: { checkBirthMonth :value => {return (value>=1 && value<=12) || "잘못된 생월 정보 입니다"},
+                                                    checkInteger: value => checkInteger(value) ||"잘못된 생월 정보 입니다"}
+                                    })}/> 
                                 </span>
                                 <span>
-                                    <input type='number' min={1} max={31} placeholder='일'/>
+                                    <input type='number' placeholder='일'  maxLength="2" onInput={numberMaxLength} {...register("birthDay",{
+                                        required: { value: true, message: "테어난 일을 입력해주세요"},
+                                        validate: { checkBirthDay :value => {return birthDayValidate(value) || "잘못된 생일 정보 입니다"},
+                                                    checkInteger: value => checkInteger(value) ||"잘못된 생일 정보 입니다"}
+                                    })}/> 
                                 </span>
                             </div>
-                            <span className='join_error_text'>잘못된 생일 정보 입니다</span>
+                            {(errors.birthYear && <span className='join_error_text'>{errors.birthYear.message}</span>) ||
+                            (errors.birthMonth && <span className='join_error_text'>{errors.birthMonth.message}</span>) ||
+                            (errors.birthDay && <span className='join_error_text'>{errors.birthDay.message}</span>)}
                         </div>
 
                         <div className='join_input_row'>
                             <span className='join_title'>Gender</span>
                             <span className='pull_input_span'>
-                                <select>
+                                <select {...register("gender")}>
                                     <option value="M">남자</option>
                                     <option value="F">여자</option>
                                 </select>
@@ -110,9 +162,12 @@ function Join(){
                         <div className='join_input_row'>
                             <span className='join_title'>Email</span>
                             <span className='pull_input_span'>
-                                <input type='email'/>
+                            <input type='text' {...register("email",{
+                                    required: {value: true, message: "Email이 입력되지 않았습니다"},
+                                    pattern: {value:/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/, message: "잘못된 email 포맷입니다"}
+                                })}/>
                             </span>
-                            <span className='join_error_text'>잘못된 이메일 정보 입니다</span>
+                            {errors.email && <span className='join_error_text'>{errors.email.message}</span>}
                         </div>
 
                         <div className='join_input_row'>
@@ -132,18 +187,21 @@ function Join(){
                                 </div>
                                 <span className='join_title'>Detail Address</span>
                                 <span className='pull_input_span'>
-                                    <input type='text'/> 
+                                    <input type='text' {...register("addressDetail")}/> 
                                 </span>
-                                <span className='join_error_text'>주소가 입력되지 않았습니다</span>
+                                {address ==="" &&<span className='join_error_text'>주소찾기를 실행해주세요</span>}
                             </div>
                         </div>
 
                         <div className='join_input_row'>
                             <span className='join_title'>Phone</span>
                             <span className='pull_input_span'>
-                                <input type='text'/>    
+                                <input type='text' {...register("phone",{
+                                        required: {value: true, message: "휴대폰 번호를 입력해주세요"},
+                                        pattern: {value:/^01([0|1|6|7|8|9])*-([0-9]{3,4})*-([0-9]{4})$/, message: "잘못된 핸드폰번호 입니다"}
+                                    })} onInput={autoHyphen} maxLength="13"/>
                             </span>
-                            <span className='join_error_text'>000-0000-0000 형식이여야 합니다</span>
+                            {errors.phone && <span className='join_error_text'>{errors.phone.message}</span>}
                         </div>
 
                         <div className='join_button_row'>
