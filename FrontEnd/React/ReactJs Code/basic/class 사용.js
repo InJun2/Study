8a4224b7775620d214676b0096358191c462da91
InJun2 <50690859+InJun2@users.noreply.github.com>
2022-05-29 22:07:43 +0900

    22-05-29
    - 주석추가

--- FrontEnd/React/ReactJs Code/basic/class 사용.js
+++ FrontEnd/React/ReactJs Code/basic/class 사용.js
@@ -2,12 +2,11 @@ import './App.css';
 import React from "react";
 import ReactDom from 'react-dom';
 
-function App() {
+function App() { /* 최근 트렌드는 클래스형 컴포넌트보다 함수형 컴포넌트 선호 ( hook으로 클래스형 컴포넌트 대체 ) */
   return (
     <div className="App">
-      <header className="App-header">
-        <Test/>
-
+      <header className="App-header"> /* 다른 클래스의 코드를 가져올 시 아래처럼 코드 사용 */
+        <Test/>  
       </header>
     </div>
   );
